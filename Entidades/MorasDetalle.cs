9a56bd3cf8d0cc7fae2a0ebad6ae3d23365e722b
Victor Johan <Vicjohan23@gmail.com>
2020-06-21 00:53:02 -0400

    Add project files.

---
+++ Entidades/MorasDetalle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RegistroPrestamoDetalle.Entidades
+{
+    public class MorasDetalle
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public int Id { get; set; }
+        public int PrestamoId { get; set; }
+        public double Valor { get; set; }
+
+        public MorasDetalle()
+        {
+            MoraId = 0;
+            Id = 0;
+            PrestamoId = 0;
+            Valor = 0;
+        }
+
+        public MorasDetalle(int id, int prestamoId, double valor)
+        {
+            MoraId = 0;
+            Id = id;
+            PrestamoId = prestamoId;
+            Valor = valor;
+        }
+    }
+}
