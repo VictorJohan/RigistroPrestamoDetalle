a64390a80f625fa49fbd5ddfaceacc14844cf32b
Victor Johan <Vicjohan23@gmail.com>
2020-06-21 01:43:24 -0400

    SE CREO LA CLASE CONTEXTO Y SE HIZO LA MIGRACION.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPrestamoDetalle.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPrestamoDetalle.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Moras> Moras { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\Prestamo.db");
+        }
+    }
+}
---
+++ Migrations/20200621054221_Migracion_Inicial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPrestamoDetalle.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moras",
+                columns: table => new
+                {
+                    MoraId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Total = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moras", x => x.MoraId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MorasDetalle",
+                columns: table => new
+                {
+                    MoraId = table.Column<int>(nullable: false),
+                    Id = table.Column<int>(nullable: false),
+                    PrestamoId = table.Column<int>(nullable: false),
+                    Valor = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MorasDetalle", x => x.MoraId);
+                    table.ForeignKey(
+                        name: "FK_MorasDetalle_Moras_MoraId",
+                        column: x => x.MoraId,
+                        principalTable: "Moras",
+                        principalColumn: "MoraId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MorasDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Moras");
+        }
+    }
+}
