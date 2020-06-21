e048065a97c0e71babaaa990886e9f67e348f18c
Victor Johan <Vicjohan23@gmail.com>
2020-06-21 01:16:02 -0400

    AGREGUE UN CONSTRUCTOR QUE HABIA OLVIDADO. En este constructor se deben inicializar las propiedades creadas en esta clase.

--- Entidades/Moras.cs
+++ Entidades/Moras.cs
@@ -10,13 +10,21 @@ namespace RegistroPrestamoDetalle.Entidades
     {
         [Key]
         public int MoraId { get; set; }
-        public DateTime Fecha { get; set; } = DateTime.Now;
+        public DateTime Fecha { get; set; }
         public double Total { get; set; }
 
         //aqui se le esta pasando una llave foranea a la clase MorasDetalle(Tabla)
         //es la Primary Key de esta misma clase(Tabla)
         [ForeignKey("MoraId")]
         public virtual List<MorasDetalle> MorasDetalles { get; set; }
+
+        public Moras()//en este constructor se deben inicializar las propiedades creadas en esta clase
+        {
+            MoraId = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            MorasDetalles = new List<MorasDetalle>();
+        }
         
     }
 }
