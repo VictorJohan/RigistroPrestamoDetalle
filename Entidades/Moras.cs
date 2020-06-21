7626e28b283e46808a453c4b4e7978ec8cc322a4
Victor Johan <Vicjohan23@gmail.com>
2020-06-21 01:10:46 -0400

    SE CREO LA ENTIDAD Moras

---
+++ Entidades/Moras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RegistroPrestamoDetalle.Entidades
+{
+    public class Moras
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public double Total { get; set; }
+
+        //aqui se le esta pasando una llave foranea a la clase MorasDetalle(Tabla)
+        //es la Primary Key de esta misma clase(Tabla)
+        [ForeignKey("MoraId")]
+        public virtual List<MorasDetalle> MorasDetalles { get; set; }
+        
+    }
+}
