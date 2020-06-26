08433dbb84fb08467106652a7a60b590f6d1499d
Victor Johan <Vicjohan23@gmail.com>
2020-06-26 02:32:47 -0400

    SE LE HIZO EL TEST Y PASARON CORECTATAMENTE. Tambien se le hicieron cambios a las proprciones del formulario.

---
+++ RegistroPrestamosDetalle/BLL/MorasBLLTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegistroPrestamoDetalle.BLL;
+using RegistroPrestamoDetalle.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPrestamoDetalle.BLL.Tests
+{
+    [TestClass()]
+    public class MorasBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Moras Mora = new Moras();
+            Mora.MoraId = 1;
+            Mora.Fecha = DateTime.Now;
+            Mora.Total = 1234;
+
+            var detalle = new MorasDetalle(1, 2, 3, 4);
+
+            Mora.MorasDetalle.Add(detalle);
+
+            Assert.IsTrue(MorasBLL.Guardar(Mora));
+        }
+
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.IsTrue(MorasBLL.Existe(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Moras encontrado = MorasBLL.Buscar(1);
+
+            Assert.IsTrue(encontrado != null);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.IsTrue(MorasBLL.Eliminar(1));
+        }
+    }
+}
